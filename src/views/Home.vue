ee0cbe481b29f842ce531a10e6885ddafe91e13d
diegogaldino-dev <68260569+diegogaldino-dev@users.noreply.github.com>
2022-09-05 15:37:15 -0300

    Create Home.vue

---
+++ src/views/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <Banner />
+  <div class="main-container">
+    <h1>Monte o seu Burger:</h1>
+    <BurgerForm />
+  </div>
+</template>
+
+<script>
+import Banner from '../components/Banner.vue';
+import BurgerForm from '@/components/BurgerForm.vue';
+  export default{
+    name: "Home",
+    components: {
+    Banner,
+    BurgerForm
+}
+}
+  
+</script>
+
